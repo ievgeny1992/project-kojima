183d0ced0105f450982f9c3ba84b09611695a405
ievgeny1992 <karbash1992@gmail.com>
2021-06-17 17:55:19 +0300

    Add notification template

---
+++ src/components/Notification.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="modal"></div>
+</template>
+
+<script>
+export default {
+  name: "Notification",
+  // props: ["name"],
+  methods: {}
+};
+</script>
+
+<style lang="scss" scoped></style>

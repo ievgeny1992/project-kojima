4d3b8a97d24e0cb1150a1466c6c6a9f3550cd351
ievgeny1992 <karbash1992@gmail.com>
2024-04-11 11:16:33 +0300

    Change genres component

--- src/components/GenresPercentLine.vue
+++ src/components/GenresPercentLine.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="genres col mt-md-3 mb-md-3 mt-2 mb-2">
     <div class="row">
-      <div class="genres__lines">
+      <div class="genres__lines" :class="{ genres__lines_hover: hover }">
         <div
           class="genre"
           v-for="(genre, index) in genres"
@@ -10,21 +10,28 @@
             backgroundColor: colors[index],
             width: getPrecent(genre.percent) + '%'
           }"
+          @mouseover="$event.target.classList.toggle('genre_active')"
+          @mouseleave="$event.target.classList.toggle('genre_active')"
         ></div>
       </div>
     </div>
     <div class="row">
       <div class="genres__tags">
         <div
-          class="genre__tag"
+          class="genre-tag"
           v-for="(genre, index) in genres"
           v-bind:key="index"
         >
           <div
-            class="genre__circle"
+            class="genre-tag__circle"
             :style="{ backgroundColor: colors[index] }"
           ></div>
-          {{ genre._id }} ({{ getPrecent(genre.percent) }}%)
+          <span class="genre-tag__name">
+            {{ genre._id }}
+          </span>
+          <span class="genre-tag__percent">
+            {{ getPrecent(genre.percent) }}%
+          </span>
         </div>
       </div>
     </div>
@@ -36,44 +43,15 @@ export default {
   name: "GenresPercentLine",
   props: {
     genres: Array,
-    color: String
+    colors: Array
   },
+
   data: function() {
     return {
-      colors: [
-        "#f78085",
-        "#c52a52",
-        "#ffdcc6",
-        "#dd4c65",
-        "#3e67ae",
-        "#79a9c9",
-        "#618fbf",
-        "#5681b9",
-        "#b61849",
-        "#ffeed3",
-        "#93003a",
-        "#a60741",
-        "#00429d",
-        "#4a74b4",
-        "#a1d1d7",
-        "#85b7ce",
-        "#6d9cc4",
-        "#ffb8ab",
-        "#c3ebde",
-        "#e75d6f",
-        "#93c4d2",
-        "#f06f7a",
-        "#305ba9",
-        "#ffcab9",
-        "#1f4ea3",
-        "#ffa59e",
-        "#fd9291",
-        "#daf7e1",
-        "#d23b5b",
-        "#b1dfdb"
-      ]
+      hover: false
     };
   },
+
   methods: {
     getPrecent: function(percent) {
       return Math.ceil(percent);
@@ -87,7 +65,6 @@ export default {
   width: 100%;
   background-color: #292933;
   border-radius: 4px;
-
   &__tags {
     display: flex;
     flex-direction: row;
@@ -105,11 +82,16 @@ export default {
   }
 }
 .genre {
-  height: 9px;
-
+  height: 11px;
   &:first-child {
     border-top-left-radius: 3px;
     border-bottom-left-radius: 3px;
+  }
+
+  &_active {
+    &:hover :not(.genre_active) {
+      background-color: #000 !important;
+    }
   }
 
   &:last-child {
@@ -117,22 +99,33 @@ export default {
     border-bottom-right-radius: 3px;
   }
 
-  &__tag {
+  &-tag {
     display: flex;
     align-items: center;
     margin-right: 7px;
     margin-bottom: 7px;
     background-color: #181818;
     border-radius: 8px;
-    padding: 3px 10px;
+    padding: 0px 10px;
     font-size: 14px;
-  }
 
-  &__circle {
-    width: 9px;
-    height: 9px;
-    border-radius: 50%;
-    margin-right: 10px;
+    &__circle {
+      width: 9px;
+      height: 9px;
+      border-radius: 50%;
+      margin-right: 5px;
+    }
+
+    &__name {
+      padding: 5px 5px;
+    }
+
+    &__percent {
+      font-weight: 900;
+      padding: 5px 0px 5px 3px;
+      margin-left: 5px;
+      // border-left: 3px solid #292933;
+    }
   }
 }
 </style>

14400e8e03f5d9ae8701f761035560ba2d54c579
ievgeny1992 <karbash1992@gmail.com>
2024-04-12 12:28:30 +0300

    New game add interface

--- src/components/GameCreator.vue
+++ src/components/GameCreator.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="row">
-    <div
-      class="col-xl-3 col-lg-4 col-md-6 pt-3 order-lg-1 order-md-1 order-sm-1 order-1"
-    >
+    <div class="col-xl-3 col-lg-4 col-md-6 pt-3">
+      <!--Serach Form-->
       <div class="search-form">
         <div class="form-group pb-3">
           <!-- <label class="search-form__label" for="name">Название игры</label> -->
@@ -38,17 +37,19 @@
           Очистить
         </button>
       </div>
+      <!--!Serach Form-->
 
+      <!--Error-->
       <div class="mt-3 alert alert-danger" role="alert" v-if="error">
         <span class="search-form__message_error"
           ><i class="fas fa-exclamation-triangle"></i> {{ error }}</span
         >
       </div>
+      <!--!Error-->
     </div>
 
-    <div
-      class="col-xl-3 col-lg-3 col-md-6 pt-3 order-lg-2 order-md-3 order-sm-3 order-3"
-    >
+    <!--Game items-->
+    <div class="col-xl-4 col-lg-5 col-md-6 pt-3">
       <transition name="fade">
         <div class="found-games" v-if="games">
           <div
@@ -58,115 +59,88 @@
             v-bind:class="{
               'found-games__item_active': selectGameIndex === index
             }"
+            v-bind:style="{
+              backgroundImage: 'url(' + game.background_image + ')'
+            }"
             v-on:click="selectGame(index)"
           >
-            <img
-              :src="game.background_image"
-              class="found-games__background"
-              alt=""
-            />
             <div class="found-games__overlay"></div>
             <div class="found-games__content">
-              <h4 class="found-games__title">
-                {{ game.name }}
-              </h4>
+              <div class="row">
+                <div class="col-12">
+                  <h4 class="found-games__title">
+                    {{ game.name }}
+                  </h4>
+                </div>
+              </div>
+              <div class="row" v-if="selectGameIndex === index">
+                <div class="col-12">
+                  <div class="input-group mb-3 mt-3">
+                    <div class="input-group-prepend">
+                      <label class="input-group-text" for="inputGroupSelect01"
+                        >Оценка</label
+                      >
+                    </div>
+                    <select
+                      v-model="currentGame.my_rating"
+                      class="form-control"
+                      id="selectMyRating"
+                    >
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4">4</option>
+                      <option value="5">5</option>
+                      <option value="6">6</option>
+                      <option value="7">7</option>
+                      <option value="8">8</option>
+                      <option value="9">9</option>
+                      <option value="10">10</option>
+                    </select>
+                  </div>
+
+                  <div class="form-group">
+                    <div class="form-check">
+                      <input
+                        class="form-check-input"
+                        type="checkbox"
+                        id="completeGameCheckbox"
+                        value="option1"
+                        v-model="currentGame.complete_flag"
+                      />
+
+                      <label class="form-check-label" for="completeGameCheckbox"
+                        >Пройдена</label
+                      >
+                    </div>
+                  </div>
+                  <div
+                    class="mt-3 alert alert-success"
+                    role="alert"
+                    v-if="added"
+                  >
+                    <span class="search-form__message_success"
+                      ><i class="fas fa-check"></i> Добавлено</span
+                    >
+                  </div>
+                  <div class="form-group" v-else>
+                    <button
+                      type="button"
+                      class="btn btn-success search-form__button"
+                      @click="addGame"
+                    >
+                      <!-- <i class="fal fa-plus"></i> -->
+                      Добавить
+                    </button>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </transition>
     </div>
-
-    <div
-      class="col-xl-4 col-lg-4 col-md-6 pt-3 order-lg-3 order-md-2 order-sm-2 order-2"
-    >
-      <transition name="fade">
-        <div
-          class="found-game"
-          v-if="currentGame"
-          v-bind:style="{
-            backgroundImage: 'url(' + currentGame.background_image__crop + ')'
-          }"
-        >
-          <div class="found-game__overlay"></div>
-          <div class="found-game__body">
-            <h3 class="pt-2">
-              {{ currentGame.name }}
-            </h3>
-
-            <div class="input-group mb-3">
-              <div class="input-group-prepend">
-                <label class="input-group-text" for="inputGroupSelect01"
-                  >Оценка</label
-                >
-              </div>
-              <select
-                v-model="currentGame.my_rating"
-                class="form-control"
-                id="selectMyRating"
-              >
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-              </select>
-            </div>
-
-            <div class="form-group">
-              <div class="form-check">
-                <input
-                  v-model="currentGame.complete_flag"
-                  class="form-check-input"
-                  type="checkbox"
-                  id="completeGameCheckbox"
-                  value="option1"
-                />
-
-                <label class="form-check-label" for="completeGameCheckbox"
-                  >Пройдена</label
-                >
-              </div>
-            </div>
-
-            <!-- <div class="form-group">
-              <div class="form-check">
-                <input
-                  v-model="currentGame.angry_flag"
-                  class="form-check-input"
-                  type="checkbox"
-                  id="angryGameCheckbox"
-                  value="option2"
-                />
-                <label class="form-check-label" for="angryGameCheckbox"
-                  >Взбесила</label
-                >
-              </div>
-            </div> -->
-
-            <div class="mt-3 alert alert-success" role="alert" v-if="added">
-              <span class="search-form__message_success"
-                ><i class="fas fa-check"></i> Добавлено</span
-              >
-            </div>
-            <div class="form-group" v-else>
-              <button
-                type="button"
-                @click="addGame"
-                class="btn btn-success search-form__button"
-              >
-                <!-- <i class="fal fa-plus"></i> -->
-                Добавить
-              </button>
-            </div>
-          </div>
-        </div>
-      </transition>
-    </div>
+    <!--!Game items-->
   </div>
 </template>
 
@@ -178,12 +152,12 @@ export default {
   components: {},
   data: function() {
     return {
-      games: null,
+      games: {},
+      currentGame: {},
       insertGame: null,
       error: null,
       added: null,
       empty: null,
-      currentGame: null,
       selectGameIndex: null,
       rawgApiKey: process.env.VUE_APP_RAWG_API_KEY
     };
@@ -209,34 +183,34 @@ export default {
     },
 
     selectGame: function(index) {
-      this.currentGame = null;
-      this.selectGameIndex = index;
+      if (this.selectGameIndex !== index) {
+        this.currentGame = this.games[index];
+        this.selectGameIndex = index;
+        this.currentGame.my_rating = 1;
+        this.currentGame.complete_flag = false;
+        this.currentGame.background_image__crop = this.getCropImage(
+          this.currentGame.background_image
+        );
 
-      const game = this.games[index];
-      const url =
-        "https://api.rawg.io/api/games/" +
-        game.slug +
-        "?key=" +
-        this.rawgApiKey;
+        const url =
+          "https://api.rawg.io/api/games/" +
+          this.currentGame.slug +
+          "?key=" +
+          this.rawgApiKey;
 
-      axios
-        .get(url)
-        .then(response => {
-          this.currentGame = response.data;
-          this.currentGame.my_rating = 1;
-          this.currentGame.complete_flag = false;
-          this.currentGame.background_image__crop = this.getCropImage(
-            this.currentGame.background_image
-          );
-        })
-        .catch(error => (this.error = error));
+        axios
+          .get(url)
+          .then(response => {
+            Object.assign(this.currentGame, response.data);
+          })
+          .catch(error => (this.error = error));
+      }
     },
 
     addGame: function() {
-      let video = this.currentGame.clip
+      const video = this.currentGame.clip
         ? this.currentGame.clip.clips.full
         : null;
-
       const addedGame = {
         name: this.currentGame.name,
         slug: this.currentGame.slug,
@@ -251,9 +225,9 @@ export default {
         completeFlag: this.currentGame.complete_flag,
         angryFlag: this.currentGame.angry_flag,
         platforms: this.currentGame.parent_platforms,
-        genres: this.currentGame.genres
+        genres: this.currentGame.genres,
+        screenshots: this.currentGame.short_screenshots
       };
-
       axios
         .post(process.env.VUE_APP_SERVER_URL + "/games", addedGame)
         .then(response => {
@@ -274,7 +248,7 @@ export default {
     },
 
     clearData: function() {
-      this.games = null;
+      this.games = {};
       this.currentGame = null;
       this.selectGameIndex = null;
       this.empty = null;
